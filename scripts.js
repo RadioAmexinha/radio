4b311b31c92a6864bbf5446e646719a5f8fefb73
RadioAmexinha <130324959+RadioAmexinha@users.noreply.github.com>
2023-06-28 00:49:11 -0300

    Site Verçao 2
    Verçao 2 do antigo site

---
+++ scripts.js
@@ -0,0 +1,104 @@
+function prog() {
+		document.getElementById("pro").style.visibility = " visible";
+		document.getElementById("pro").style.position = "relative";
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("ajude").style.visibility = "hidden";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
+
+function cont() {
+		document.getElementById("con").style.visibility = " visible";
+		document.getElementById("con").style.position = "relative";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("ajude").style.visibility = "hidden";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
+
+function aju() {
+		document.getElementById("ajude").style.visibility = " visible";
+		document.getElementById("ajude").style.position = "relative";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
+
+function sobr() {
+		document.getElementById("sobre").style.visibility = " visible";
+		document.getElementById("sobre").style.position = "relative";
+		document.getElementById("ajude").style.visibility = " hidden";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
+
+function esp() {
+		document.getElementById("espe").style.visibility = " visible";
+		document.getElementById("espe").style.position = "relative";
+		document.getElementById("ajude").style.visibility = " hidden";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
+
+function gast() {
+		document.getElementById("gastos").style.visibility = " visible";
+		document.getElementById("gastos").style.position = "relative";
+		document.getElementById("ajude").style.visibility = " hidden";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		}
+		
+function ini() {
+		document.getElementById("con").style.visibility = "hidden";
+		document.getElementById("pro").style.visibility = "hidden";
+		document.getElementById("ajude").style.visibility = "hidden";
+		document.getElementById("con").style.position = "absolute";
+		document.getElementById("pro").style.position = "absolute";
+		document.getElementById("ajude").style.position = "absolute";
+		document.getElementById("sobre").style.visibility = " hidden";
+		document.getElementById("sobre").style.position = "absolute";
+		document.getElementById("espe").style.visibility = " hidden";
+		document.getElementById("espe").style.position = "absolute";
+		document.getElementById("gastos").style.visibility = " hidden";
+		document.getElementById("gastos").style.position = "absolute";
+		}
